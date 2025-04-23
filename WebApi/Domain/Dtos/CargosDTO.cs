ee687a20e9ed2d991cbd656b3ec7e28e6ed87663
Conrado Capistrano <capistrano.conrado@gmail.com>
2025-04-23 17:52:34 -0300

    build: criar entidade Cargos.

---
+++ WebApi/Domain/Dtos/CargosDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Dtos
+{
+    class CargosDTO
+    {
+        [Required(ErrorMessage = "O Titulo é obrigatório.")]
+        [MaxLength(50, ErrorMessage = "Máximo de 50 caracteres para Titulo.")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        [MaxLength(250, ErrorMessage = "Máximo de 250 caracteres para Descrição.")]
+        public string Descricao { get; set; }
+        [Required(ErrorMessage = "O salário base é obrigatório.")]
+        [Range(2000.0, double.MaxValue, ErrorMessage = "O salário base deve ser no mínimo 2000,00.")]
+        public float SalarioBase { get; set; }
+    }
+}
---
+++ WebApi/Domain/Models/Cargos.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Models
+{
+    class Cargos
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public float SalarioBase { get; set; }
+    }
+}
