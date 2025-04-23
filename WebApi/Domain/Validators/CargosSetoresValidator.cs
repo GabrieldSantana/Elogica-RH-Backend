aa9ca4cdee9cc6f0e918ee41c691fca4ec87da8c
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-23 16:35:53 -0300

    feat: criar validator cargosSetores

---
+++ WebApi/Domain/Validators/CargosSetoresValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+using FluentValidation;
+
+namespace Domain.Validators
+{
+    public class CargosSetoresValidator :  AbstractValidator<CargosSetores>
+    {
+        public CargosSetoresValidator()
+        {
+            RuleFor(x => x.CargosId).NotEmpty();
+            RuleFor(x => x.SetoresId).NotEmpty();
+            
+
+        }
+    }
+}
