3e0d8bb2dd5afce2999b7a7aaae07aea0ddd8f9d
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-23 17:47:44 -0300

    feat: obter, deletar, paginado e por id

---
+++ WebApi/Infrastructure/Repositories/SetorRepository.cs
@@ -0,0 +1,100 @@
+﻿using System.Data;
+using Dapper;
+using Domain.Models;
+using Infrastructure.Interfaces;
+
+namespace Infrastructure.Repositories;
+
+internal class SetorRepository : ISetorRepository
+{
+    private readonly IDbConnection _connection;
+    public SetorRepository(IDbConnection connection)
+    {
+        _connection = connection;
+    }
+    public async Task<Setor> AdicionarSetoresAsync(Setor setor)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Setor> AtualizarSetoresAsync(Setor setor)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Setor> ObterSetoresPorIdAsync(int id)
+    {
+        try
+        {
+            string sql = $"SELECT * FROM Setores WHERE Id={id}";
+            var setor = await _connection.QueryFirstOrDefaultAsync<Setor>(sql);
+            if (setor == null)
+            {
+                throw new Exception("Setor não encontrado");
+            }
+            return setor;
+        }
+        catch(Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task<RetornoPaginado<Setor>> ObterSetoresPorPaginaAsync(int pagina, int quantidade)
+    {
+        try
+        {
+            string sql = "SELECT * FROM SETORES ORDER BY ID OFFSET @OFFSET ROWS FETCH NEXT @QUANTIDADE ROWS ONLY";
+
+            var parametros = new
+            {
+                OFFSET = (pagina - 1) * quantidade,
+                QUANTIDADE = quantidade
+            };
+
+            var setores = await _connection.QueryAsync<Setor>(sql, parametros);
+            var totalSetores = "SELECT COUNT(*) FROM Setores";
+            var retornoTotalSetores = await _connection.ExecuteScalarAsync<int>(totalSetores);
+            var retornoPaginado = new RetornoPaginado<Setor>
+            {
+                TotalRegistros = retornoTotalSetores,
+                Pagina = pagina,
+                QtdPagina = quantidade,
+                Retorno = setores.ToList()
+            };
+            return retornoPaginado;
+        }
+        catch(Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task<IEnumerable<Setor>> ObterTodosSetoresAsync()
+    {
+        try
+        {
+        string sql = "SELECT * FROM Setores";
+        var setores = await _connection.QueryAsync<Setor>(sql);
+        return setores;
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task<bool> RemoverSetoresAsync(int id)
+    {
+        try {
+            string sql = "DELETE FROM Setores WHERE Id = @Id";
+            var parameters = new { Id = id };
+            var result = await _connection.ExecuteAsync(sql, parameters);
+            return result > 0;
+        }
+        catch (Exception)
+        {
+            throw;
+        };
+    }
+}
