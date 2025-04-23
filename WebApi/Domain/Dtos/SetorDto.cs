bf1af1cbbbe26bd3dd10e206d74213a6c6b321ec
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-23 16:31:54 -0300

    feat: adicionar validacoes diretamente no dto

--- WebApi/Domain/Dtos/SetorDto.cs
+++ WebApi/Domain/Dtos/SetorDto.cs
@@ -1,8 +1,13 @@
-﻿namespace Domain.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Dtos;
+
+public class SetorDto
 {
-    internal class SetorDto
-    {
-        public string Nome { get; set; }
-        public string Descricao { get; set; }
-    }
+    [Required(ErrorMessage = "O campo nome é obrigatório.")]
+    [MaxLength(50, ErrorMessage = "A descrição deve ter no máximo 50 caracteres.")]
+    public string Nome { get; set; }
+    [Required(ErrorMessage = "O campo descrição é obrigatório.")]
+    [MaxLength(250, ErrorMessage = "A descrição deve ter no máximo 250 caracteres.")]
+    public string Descricao { get; set; }
 }
