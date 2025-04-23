dc00e6115b20e70fe2b5c7d948b5f0b691c3760f
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-23 17:05:40 -0300

    feat: adicionar retornoPaginado Generico

---
+++ WebApi/Domain/Models/RetornoPaginado.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain;
+
+public class RetornoPaginado<T> where T : class
+{
+    public int TotalRegistro { get; set; }
+    public int Quantidade { get; set; }
+    public int Pagina { get; set; }
+
+    public List<T> RetornoPagina { get; set; }
+}
