cb3adf371d5017ac95c3a04aa36262680fa92917
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-23 16:27:46 -0300

    feat: criar modelo de setor

---
+++ WebApi/Domain/Models/Setor.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models;
+
+public class Setor
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+}
