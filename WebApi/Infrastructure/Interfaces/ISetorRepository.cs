306f607be6036c8f29e81ad7d520a8b0359a10a2
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-23 17:09:23 -0300

    feat: criar a interface que sera herdada pelo setorrepository

--- WebApi/Infrastructure/Interfaces/ISetorRepository.cs
+++ WebApi/Infrastructure/Interfaces/ISetorRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace Infrastructure.Interfaces;
+﻿using Domain.Models;
+
+namespace Infrastructure.Interfaces;
 
 public interface ISetorRepository
 {
-
+    Task<IEnumerable<Setor>> ObterTodosSetoresAsync();
+    Task<RetornoPaginado<Setor>> ObterSetoresPorPagina(int pagina, int quantidade);
+    Task<Setor> ObterSetoresPorIdAsync(int id);
+    Task<Setor> AdicionarSetoresAsync(Setor setor);
+    Task<Setor> AtualizarSetoresAsync(Setor setor);
+    Task<bool> RemoverSetoresAsync(int id);
+  
 }
