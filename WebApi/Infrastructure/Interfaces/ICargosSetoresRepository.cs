f551c81ccbaa7f2526ad94d39982385e5a080f1b
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-23 16:21:49 -0300

    feat: adicionar metodo CargosSetores InterfaceRepositorie

--- WebApi/Infrastructure/Interfaces/ICargosSetoresRepository.cs
+++ WebApi/Infrastructure/Interfaces/ICargosSetoresRepository.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Models;
 
 namespace Infrastructure.Interfaces
 {
     public interface ICargosSetoresRepository
     {
-        Task<bool> RetornarTodosCargosSetores();
-        
+        Task<bool> AtualizarCargosSetores();
+        Task<List<CargosSetores>> RetornarTodosCargosSetores();
+        Task<int> AdicionarCargosSetores();
+        Task<bool> DeletarCargosSetores();
+
+
         
     }
 }
