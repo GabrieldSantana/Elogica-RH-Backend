41950a81ef0c95d6ea8c6dfe888d162626286b43
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-24 16:26:41 -0300

    fix: ajustar paramentro deletarCargosSetores

--- WebApi/Application/Interfaces/ICargosSetoresService.cs
+++ WebApi/Application/Interfaces/ICargosSetoresService.cs
@@ -8,7 +8,7 @@ namespace Application.Interfaces
         Task<bool> AtualizarCargosSetoresAsync(CargosSetores cargosSetores);
         Task<List<CargosSetores>> BuscarCargosSetoresAsync();
         Task<int> AdicionarCargosSetoresAsync(CargosSetores cargosSetores);
-        Task<bool> ExcluirCargosSetoresAsync(CargosSetores cargosSetores);
+        Task<bool> ExcluirCargosSetoresAsync(int cargosId);
 
         Task<RetornoPaginado<CargosSetores>> BuscarCargosSetoresPaginadoAsync(int quantidade, int pagina);
     }
