9e0f1ab5cba8fb58be46486d5b5bb127be49e565
Vanvrs <vanessrodrigues@hotmail.com>
2025-04-24 12:52:33 -0300

    feat: Adicionar ao Domain Férias.

---
+++ WebApi/Domain/Models/Ferias.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models;
+
+public class Ferias
+{
+    public int Id { get; set; }
+    public DateTime DataInicio { get; set; }
+    public DateTime DataFim { get; set; }
+    public int FuncionarioId { get; set; }  
+    public Funcionario Funcionario { get; set; }
+}
