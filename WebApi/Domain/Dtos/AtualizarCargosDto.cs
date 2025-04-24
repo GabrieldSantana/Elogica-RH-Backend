d04ef721530d4f9f8ef3af5c7a250e073c619ea3
Conrado Capistrano <capistrano.conrado@gmail.com>
2025-04-24 20:33:31 -0300

    Build: Criar Dto para atualizacao de Cargo.

---
+++ WebApi/Domain/Dtos/AtualizarCargosDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Dtos
+{
+    public class AtualizarCargosDto
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "Máximo de 50 caracteres para Titulo.")]
+        public string Titulo { get; set; }
+        [Required]
+        [MaxLength(250, ErrorMessage = "Máximo de 250 caracteres para Descrição.")]
+        public string Descricao { get; set; }
+        [Required]
+        [Range(2000.0, double.MaxValue, ErrorMessage = "O salário base deve ser no mínimo 2000,00.")]
+        public float SalarioBase { get; set; }
+    }
+}
