6d561d22990ce397f059486573156beb65dd6e99
Conrado Capistrano <capistrano.conrado@gmail.com>
2025-04-24 15:41:57 -0300

    Refact: Alterar logica do BuscarCargos.

--- WebApi/Infrastructure/Interfaces/ICargosRepository.cs
+++ WebApi/Infrastructure/Interfaces/ICargosRepository.cs
@@ -5,7 +5,7 @@ namespace Infrastructure.Interfaces
     interface ICargosRepository
     {
         Task<RetornoPaginado<Cargos>> BuscarCargosPaginadoAsync(int pagina, int quantidade);
-        Task<Cargos> BuscarCargosAsync();
+        Task<IEnumerable<Cargos>> BuscarCargosAsync();
         Task<Cargos> BuscarCargosPorIdAsync(int id);
         Task<int> AdicionarCargosAsync(Cargos cargos);
         Task<bool> AtualizarCargosAsync(Cargos cargos);
