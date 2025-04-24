f4df43846cd1d9e02ec2e75c629332bf05b10a36
Conrado Capistrano <capistrano.conrado@gmail.com>
2025-04-24 12:38:46 -0300

    Build: criar model de Cargos.

--- WebApi/Domain/Models/Cargos.cs
+++ WebApi/Domain/Models/Cargos.cs
@@ -1,10 +1,11 @@
 ﻿namespace Domain.Models
 {
-    class Cargos
+    public class Cargos
     {
         public int Id { get; set; }
         public string Titulo { get; set; }
         public string Descricao { get; set; }
         public float SalarioBase { get; set; }
+        public List<Setor> Setores { get; set; } = new List<Setor>();
     }
 }
