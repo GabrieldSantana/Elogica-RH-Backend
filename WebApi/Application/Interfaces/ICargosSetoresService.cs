91782d38bb037eda6a40585f828ba3c510dda2f7
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-24 15:30:19 -0300

    Feat: adicionar metodos service

---
+++ WebApi/Application/Interfaces/ICargosSetoresService.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+
+namespace Application.Interfaces
+{
+    public interface ICargosSetoresService
+    {
+
+        Task<bool> AtualizarCargosSetoresAsync(CargosSetores cargosSetores);
+        Task<List<CargosSetores>> BuscarCargosSetoresAsync();
+        Task<int> AdicionarCargosSetoresAsync(CargosSetores cargosSetores);
+        Task<bool> ExcluirCargosSetoresAsync(CargosSetores cargosSetores);
+
+        Task<RetornoPaginado<CargosSetores>> BuscarCargosSetoresPaginadoAsync(int quantidade, int pagina);
+    }
+}
