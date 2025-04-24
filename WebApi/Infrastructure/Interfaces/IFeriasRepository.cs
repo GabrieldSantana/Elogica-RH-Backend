888ad87c450ddedb3200769322c7e87156400dae
Vanvrs <vanessrodrigues@hotmail.com>
2025-04-24 16:02:02 -0300

    feat: Adicionar IFerias Repository

---
+++ WebApi/Infrastructure/Interfaces/IFeriasRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+
+
+namespace Infrastructure.Interfaces
+{
+    public interface IFeriasRepository
+    {
+        Task<IEnumerable<Ferias>> BuscarFeriasAsync();
+        Task<RetornoPaginado<Ferias>> BuscarFeriasPaginadoAsync(int pagina, int quantidade);
+        Task<Ferias> BuscarFeriasPorIdAsync(int id);
+        Task<Ferias> AdicionarFeriasAsync(Ferias ferias);
+        Task AtualizarFeriasAsync(Ferias ferias);
+        Task ExcluirFeriasAsync(int id);
+        Task<bool> FuncionarioTemFerias(int funcionarioId, DateTime dataInicio, DateTime dataFim);
+    }
+}
