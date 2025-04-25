88441441689cacb928cb5397a733b8a7e58a5b00
Dávisson Falcão <davissonbeat@hotmail.com>
2025-04-25 12:33:32 -0300

    feat: subir pr

--- WebApi/Elogica-RH/Config/DependencyInjection.cs
+++ WebApi/Elogica-RH/Config/DependencyInjection.cs
@@ -14,6 +14,7 @@ public static class DependencyInjection
         services.AddScoped<INotificador, Notificador>();
 
 
+
         #region Services
         services.AddScoped<ISetorService, SetorService>();
         #endregion
