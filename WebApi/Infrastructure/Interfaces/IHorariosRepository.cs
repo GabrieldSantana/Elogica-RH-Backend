9000ae3df9986a09946e0c7b64a152b590dc16cb
mclaraoliveira <clara.santoss.oliveira@gmail.com>
2025-04-25 12:54:03 -0300

    feat: Criar repository e interface de Horários na pasta Infrastructure

---
+++ WebApi/Infrastructure/Interfaces/IHorariosRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Dtos;
+using Domain.Models;
+
+namespace Infrastructure.Interfaces
+{
+    public interface IHorariosRepository
+    {
+        Task<List<Horarios>> BuscarTodosHorariosAsync();
+        Task<Horarios> BuscarHorarioPorIdAsync(int id);
+        Task<RetornoPaginado<Horarios>> BuscarHorarioPaginadoAsync(int pagina, int quantidade);
+        Task<bool> AdicionarHorarioAsync(HorariosDto horario);
+        Task<bool> AtualizarHorarioAsync(int id, HorariosDto horario);
+        Task<bool> ExcluirHorarioAsync(int id);
+    }
+}
