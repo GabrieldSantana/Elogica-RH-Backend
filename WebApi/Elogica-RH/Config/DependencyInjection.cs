f99991f209a9ed139976717d8951e87cf22d2f28
Gabriel de Santana <162528876+GabrieldSantana@users.noreply.github.com>
2025-04-25 13:44:23 -0300

    feat: Adicionar conteúdo da main na branch atual

--- WebApi/Elogica-RH/Config/DependencyInjection.cs
+++ WebApi/Elogica-RH/Config/DependencyInjection.cs
@@ -5,7 +5,7 @@ using Infrastructure.Repositories;
 
 namespace Elogica_RH.Config;
 
-public class DependencyInjection
+public static class DependencyInjection
 {
     public static IServiceCollection DependencInjection(this IServiceCollection services, IConfiguration configuration)
     {
@@ -18,7 +18,14 @@ public class DependencyInjection
         services.AddScoped<IHorariosRepository, HorariosRepository>();
         services.AddScoped<IHorariosService, HorariosService>();
 
-
+        #region Cargos
+        #region Service
+        services.AddScoped<ICargosServices, CargosService>();
+        #endregion
+        #region Repository
+        services.AddScoped<ICargosRepository, CargosRepository>();
+        #endregion
+        #endregion
 
         #region Services
         services.AddScoped<ISetorService, SetorService>();
@@ -27,6 +34,7 @@ public class DependencyInjection
         #region Repositories
         services.AddScoped<ISetorRepository, SetorRepository>();
         #endregion
+
         return services;
     }
 }
