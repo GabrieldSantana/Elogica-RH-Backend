05aa9a5e7d54cc99480672369fcf073b7bb4d96a
eltonluiz178 <eltonluiz533@gmail.com>
2025-04-25 14:19:21 -0300

    fix: Retornando um cargo em vez de Dto para mostrar o id.

--- WebApi/Application/Interfaces/ICargosServices.cs
+++ WebApi/Application/Interfaces/ICargosServices.cs
@@ -6,7 +6,7 @@ namespace Application.Interfaces
     public interface ICargosServices
     {
         Task<RetornoPaginado<Cargos>> BuscarCargosPaginadoAsync(int pagina, int quantidade);
-        Task<IEnumerable<CargosDto>> BuscarCargosAsync();
+        Task<IEnumerable<Cargos>> BuscarCargosAsync();
         Task<Cargos> BuscarCargosPorIdAsync(int id);
         Task<int> AdicionarCargosAsync(CargosDto cargosDto);
         Task<bool> AtualizarCargosAsync(int id, AtualizarCargosDto cargosDto);
