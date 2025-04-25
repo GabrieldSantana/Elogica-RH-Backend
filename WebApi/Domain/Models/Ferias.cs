ff0abe2909340ac8aaafb9d34aaf71690d385863
Vanvrs <vanessrodrigues@hotmail.com>
2025-04-25 15:45:41 -0300

    fix: Modificar Para pegar Id Funcionário

--- WebApi/Domain/Models/Ferias.cs
+++ WebApi/Domain/Models/Ferias.cs
@@ -5,6 +5,6 @@ public class Ferias
     public int Id { get; set; }
     public DateTime DataInicio { get; set; }
     public DateTime DataFim { get; set; }
-    public int FuncionarioId { get; set; }  
-    public Funcionario Funcionario { get; set; }
+    public int FuncionarioId { get; set; }
+
 }
